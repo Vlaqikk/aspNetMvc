0630e436c2a919ef2be44e1cf55a20d2a4833b6b
Vlaqikk <ilinskyvladislav2002@mail.ru>
2024-09-30 00:56:23 +0500

    V3

---
+++ myProj/Migrations/202409291936414_Create.cs
@@ -0,0 +1,40 @@
+﻿namespace myProj.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Create : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CompaniesBranches",
+                c => new
+                    {
+                        Name = c.String(nullable: false, maxLength: 128),
+                        CompanyId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Name)
+                .ForeignKey("dbo.Companies", t => t.CompanyId)
+                .Index(t => t.CompanyId);
+            
+            CreateTable(
+                "dbo.Companies",
+                c => new
+                    {
+                        Name = c.String(nullable: false, maxLength: 128),
+                        Kind = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Name);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.CompaniesBranches", "CompanyId", "dbo.Companies");
+            DropIndex("dbo.CompaniesBranches", new[] { "CompanyId" });
+            DropTable("dbo.Companies");
+            DropTable("dbo.CompaniesBranches");
+        }
+    }
+}
